2b19c5ead8d511fb50c026bed42d91a89244701b
sfted <someone.fte@gmail.com>
2021-06-02 13:15:23 +0300

    Добавил возможность создания облложки из файла

--- Core/Entities/Cover.cs
+++ Core/Entities/Cover.cs
@@ -1,7 +1,22 @@
-﻿namespace Player.Core.Entities
+﻿using System.Drawing;
+using System.IO;
+
+namespace Player.Core.Entities
 {
     public class Cover : BaseEntity
     {
         public byte[] Data { get; set; }
+
+        public static Cover FromFileName(string filename)
+        {
+            Image img = Image.FromFile(filename);
+            byte[] data;
+            using (var stream = new MemoryStream())
+            {
+                img.Save(stream, System.Drawing.Imaging.ImageFormat.Jpeg);
+                data = stream.ToArray();
+            }
+            return new Cover { Data = data };
+        }
     }
 }
